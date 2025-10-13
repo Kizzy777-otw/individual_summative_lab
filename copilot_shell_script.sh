b5ecd302b0655d2b78c7d69cde4cd18b3bf96059
Kizzy777-otw <i.kizito@alustudent.com>
2025-10-13 20:07:23 +0200

    Removed submission_reminder_Kizito from main branch

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 read -p "Enter the assignment name: " new_assignment
 #replacing old assignment name with the new one
-sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"$new_assignment\"/" ./config/config.env
+sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"$new_assignment\"/" ./submission_reminder_Kizito/config/config.env
+
 #confirming changes
 echo "The assignment is now '$new_assignment' in config/config.env."
 
--- submission_reminder_Kizito/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-source ./config/config.env
-source ./modules/functions.sh
-bash ./app/reminder.sh
-
